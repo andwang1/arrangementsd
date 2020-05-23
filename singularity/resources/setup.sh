c7736a6c79320f7f8f769de2457b036d1be0590b
Andy Wang <aw1912@ic.ac.uk>
2020-05-23 11:27:23 +0100

    changing names

--- singularity/resources/setup.sh
+++ singularity/resources/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-./waf configure --exp example-pytorch-sferes --cpp14=yes --kdtree /workspace/include
+./waf configure --exp balltrajectorysd --cpp14=yes --kdtree /workspace/include
 
-./waf --exp example-pytorch-sferes -j 1
+./waf --exp balltrajectorysd -j 1
 echo 'FINISHED BUILDING. Now fixing name of files'
-python -m fix_build --path-folder /git/sferes2/build/exp/example-pytorch-sferes/
+python -m fix_build --path-folder /git/sferes2/build/exp/balltrajectorysd/
