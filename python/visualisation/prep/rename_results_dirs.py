2d4859e5ae1500883f2b71185bf5ed39248d34b5
Andy Wang <aw1912@ic.ac.uk>
2020-06-24 16:56:39 +0100

    adding new file extensions to rename script

--- python/visualisation/prep/rename_results_dirs.py
+++ python/visualisation/prep/rename_results_dirs.py
@@ -1,9 +1,9 @@
 import os
-variant = "vae"
+variant = "aurora"
 
-BASE_NAME = "/home/andwang1/airl/balltrajectorysd/results_box2d_exp1/box2dtest/params_change_start"
+BASE_NAME = "/home/andwang1/airl/imagesd/test_results/new_traj_stat"
 
-os.chdir(f"{BASE_NAME}/results_balltrajectorysd_{variant}")
+os.chdir(f"{BASE_NAME}/results_imagesd_{variant}")
 dir_names = os.listdir()
 
 for name in dir_names:
@@ -12,7 +12,6 @@ for name in dir_names:
 
     components = name.split("_")
     args = [i.split("=")[-1] for i in components]
-    # new_name = f"gen{args[0]}_random{args[1]}_fullloss{args[2]}"
-    new_name = f"gen{args[0]}_random{args[1]}_fullloss{args[2]}_beta{args[3]}_extension{args[4]}"
+    new_name = f"gen{args[0]}_random{args[1]}_fullloss{args[2]}_beta{args[3]}_extension{args[4]}_lossfunc{args[5]}_sigmoid{args[6]}"
 
     os.rename(name, new_name)
