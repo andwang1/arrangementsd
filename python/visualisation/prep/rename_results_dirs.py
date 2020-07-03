943454bdcbd196f695f3c8f773966b04b937187d
Andy Wang <aw1912@ic.ac.uk>
2020-07-03 09:53:55 +0100

    updating rename results dir for new cmd line arg

--- python/visualisation/prep/rename_results_dirs.py
+++ python/visualisation/prep/rename_results_dirs.py
@@ -12,6 +12,6 @@ for name in dir_names:
 
     components = name.split("_")
     args = [i.split("=")[-1] for i in components]
-    new_name = f"gen{args[0]}_random{args[1]}_fullloss{args[2]}_beta{args[3]}_extension{args[4]}_lossfunc{args[5]}_sigmoid{args[6]}"
+    new_name = f"gen{args[0]}_random{args[1]}_fullloss{args[2]}_beta{args[3]}_extension{args[4]}_lossfunc{args[5]}_sigmoid{args[6]}_sample{args[7]}"
 
     os.rename(name, new_name)
