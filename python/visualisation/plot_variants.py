944e48a6a9ffd596fbd2191b958eb954bfe796e9
Andy Wang <aw1912@ic.ac.uk>
2020-08-10 18:03:28 +0100

    deleting empty keys

--- python/visualisation/plot_variants.py
+++ python/visualisation/plot_variants.py
@@ -9,13 +9,11 @@ path = "/media/andwang1/SAMSUNG/MSC_INDIV/results_box2d_imagesd_exp1"
 os.chdir(path)
 
 plotting_groups = [
-["sigmoidbce", "l2"],
-# ["l1", "l2"],
-# ["l2withsampling", "l2"],
-["huberbeta0", "huberbeta1"],
-# ["l2", "l2beta0"],
-# ["l1", "l1beta0"],
-# ["huberbeta0", "l1beta0", "l2beta0"]
+    # ["l2withsampling", "l2withsampling_beta0"],
+    # ["l2", "l2withsampling"],
+    # ["l2", "l2beta0"],
+    ["l2_nosampletrain", "l2"],
+    # ["l2_nosampletrain", "l2beta0_nosampletrain"],
 ]
 
 colours = ["blue", "brown", "grey", "green", "purple", "red", "pink", "orange"]
@@ -43,14 +41,17 @@ for group in plotting_groups:
         for variant, data in log_data.items():
             if "aurora" in variant:
                 continue
+            empty_keys = [k for k in data if not len(data[k])]
+            for k in empty_keys:
+                del data[k]
             variant_name = variant if not variant.startswith("ae") else "ae"
             sns.lineplot(data["stoch"], data["UL"], estimator=np.median, ci=None, label=f"{member}-{variant_name}",
                          ax=ax1, color=colours[colour_count])
             if i == 0 and len(group) > 1:
                 ax1.lines[-1].set_linestyle("--")
-            data_stats = pd.DataFrame(data)[["stoch", "AL"]].groupby("stoch").describe()
-            quart25 = data_stats[('AL', '25%')]
-            quart75 = data_stats[('AL', '75%')]
+            data_stats = pd.DataFrame(data)[["stoch", "UL"]].groupby("stoch").describe()
+            quart25 = data_stats[("UL", '25%')]
+            quart75 = data_stats[("UL", '75%')]
             ax1.fill_between([0, 1, 2, 3, 4, 5], quart25, quart75, alpha=0.3, color=colours[colour_count])
             if i == 0 and len(group) > 1:
                 ax1.lines[-1].set_linestyle("--")
