1f5ac2964e27670e8cddaadb7eba76575c2d2661
Andy Wang <aw1912@ic.ac.uk>
2020-09-16 21:36:39 +0100

    updating visualisation

--- python/visualisation/visualise_images_container.py
+++ python/visualisation/visualise_images_container.py
@@ -67,58 +67,105 @@ if "vae" in FILE:
         # both kwargs together make the box squared
         ax1 = f.add_subplot(spec[0, 0], aspect='equal', adjustable='box')
         prediction = indiv[0]
-        prediction = np.array(prediction).reshape((DISCRETISATION, DISCRETISATION))
-        ax1.imshow(prediction, vmin=0, vmax=1, cmap="gray")
+
+        x = []
+        y = []
+        counter_x = 0
+        counter_y = 0
+        for entry in prediction:
+            if counter_x >= DISCRETISATION:
+                counter_x = 0
+                counter_y += 1
+                if counter_y >= DISCRETISATION:
+                    counter_y = 0
+
+            if entry >= 0.5:
+                x.append(counter_x * (ROOM_W / DISCRETISATION))
+                y.append(counter_y * (ROOM_H / DISCRETISATION))
+            counter_x += 1
 
         ax1.set_ylim([0, ROOM_H])
         ax1.set_xlim([0, ROOM_W])
+        ax1.scatter(x, y)
 
-        actual = indiv[1]
         ax2 = f.add_subplot(spec[0, 1], aspect='equal', adjustable='box')
+        actual = indiv[1]
+        x = []
+        y = []
+        counter_x = 0
+        counter_y = 0
+        for entry in actual:
+            if counter_x >= DISCRETISATION:
+                counter_x = 0
+                counter_y += 1
+                if counter_y >= DISCRETISATION:
+                    counter_y = 0
 
-        actual = np.array(actual).reshape((DISCRETISATION, DISCRETISATION))
-        ax2.imshow(actual, vmin=0, vmax=1, cmap="gray")
+            if entry == 1:
+                x.append(counter_x * (ROOM_W / DISCRETISATION))
+                y.append(counter_y * (ROOM_H / DISCRETISATION))
+            counter_x += 1
 
         ax2.set_ylim([0, ROOM_H])
         ax2.set_xlim([0, ROOM_W])
-        ax2.legend(loc="best")
+        ax2.scatter(x, y)
 
         noise_free_actual = indiv[2]
         ax3 = f.add_subplot(spec[0, 2], aspect='equal', adjustable='box')
 
-        noise_free_actual = np.array(noise_free_actual).reshape((DISCRETISATION, DISCRETISATION))
-        ax3.imshow(noise_free_actual, vmin=0, vmax=1, cmap="gray")
+        x = []
+        y = []
+        counter_x = 0
+        counter_y = 0
+        for entry in noise_free_actual:
+            if counter_x >= DISCRETISATION:
+                counter_x = 0
+                counter_y += 1
+                if counter_y >= DISCRETISATION:
+                    counter_y = 0
+
+            if entry == 1:
+                x.append(counter_x * (ROOM_W / DISCRETISATION))
+                y.append(counter_y * (ROOM_H / DISCRETISATION))
+            counter_x += 1
 
         ax3.set_ylim([0, ROOM_H])
         ax3.set_xlim([0, ROOM_W])
+        ax3.scatter(x, y)
 
         ax1.set_title("Construction")
         ax2.set_title("Actual Observation")
         ax3.set_title("Noise-Free Observation")
 
         L2 = np.array(indiv[3]).reshape(DISCRETISATION, DISCRETISATION)
-        ax3 = f.add_subplot(spec[1, 0], aspect='equal', adjustable='box')
-        divider1 = make_axes_locatable(ax3)
+        ax4 = f.add_subplot(spec[1, 0], aspect='equal', adjustable='box')
+        divider1 = make_axes_locatable(ax4)
         cax1 = divider1.append_axes("left", size="5%", pad=0.45)
         cax1.yaxis.set_ticks_position('left')
         cax1.yaxis.set_label_position('left')
-        sns.heatmap(L2, ax=ax3, vmin=0, cbar_ax=cax1,
+        sns.heatmap(L2, ax=ax4, vmin=0, cbar_ax=cax1,
                     xticklabels=np.arange(0, ROOM_W, ROOM_W / DISCRETISATION),
                     yticklabels=np.arange(0, ROOM_H, ROOM_H / DISCRETISATION))
         cax1.yaxis.set_ticks_position('left')
         cax1.yaxis.set_label_position('left')
-        ax3.set_title("L2")
-        ax3.invert_yaxis()
+        ax4.set_title("L2")
+        # hide ticks
+        ax4.axes.xaxis.set_visible(False)
+        ax4.axes.yaxis.set_visible(False)
+        ax4.invert_yaxis()
 
         var = np.array(indiv[-1]).reshape(DISCRETISATION, DISCRETISATION)
-        ax4 = f.add_subplot(spec[1, 1], aspect='equal', adjustable='box')
-        divider2 = make_axes_locatable(ax4)
+        ax5 = f.add_subplot(spec[1, 1], aspect='equal', adjustable='box')
+        divider2 = make_axes_locatable(ax5)
         cax2 = divider2.append_axes("right", size="5%", pad=0.45)
-        sns.heatmap(var, ax=ax4, vmin=0, cbar_ax=cax2,
+        sns.heatmap(var, ax=ax5, vmin=0, cbar_ax=cax2,
                     xticklabels=np.arange(0, ROOM_W, ROOM_W / DISCRETISATION),
                     yticklabels=np.arange(0, ROOM_H, ROOM_H / DISCRETISATION))
-        ax4.set_title("Variance")
-        ax4.invert_yaxis()
+        ax5.set_title("Variance")
+        # hide ticks
+        ax5.axes.xaxis.set_visible(False)
+        ax5.axes.yaxis.set_visible(False)
+        ax5.invert_yaxis()
 
         plt.subplots_adjust(hspace=0.6)
         plt.savefig(f"image_{j}.png")
@@ -172,14 +219,17 @@ else:
                 column = []
 
         L2 = np.array(indiv[3]).reshape(DISCRETISATION, DISCRETISATION)
-        ax3 = f.add_subplot(spec[1, 1], aspect='equal', adjustable='box')
-        divider = make_axes_locatable(ax3)
+        ax4 = f.add_subplot(spec[1, 1], aspect='equal', adjustable='box')
+        divider = make_axes_locatable(ax4)
         cax = divider.append_axes("right", size="5%", pad=0.05)
-        sns.heatmap(L2, ax=ax3, vmin=0, vmax=1, cbar_ax=cax,
+        sns.heatmap(L2, ax=ax4, vmin=0, vmax=1, cbar_ax=cax,
                     xticklabels=np.arange(0, ROOM_W, ROOM_W / DISCRETISATION),
                     yticklabels=np.arange(0, ROOM_H, ROOM_H / DISCRETISATION))
-        ax3.invert_yaxis()
-        ax3.set_title("L2")
+        ax4.invert_yaxis()
+        # hide ticks
+        ax4.axes.xaxis.set_visible(False)
+        ax4.axes.yaxis.set_visible(False)
+        ax4.set_title("L2")
 
         plt.subplots_adjust(hspace=0.6)
         plt.savefig(f"image_{j}.png")
