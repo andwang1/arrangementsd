c961da7350db2aafcf0d1be83a67b15eeb6e5128
Andy Wang <aw1912@ic.ac.uk>
2020-09-16 21:06:59 +0100

    correcting is moved in latent space to look at the undisturbed image

--- python/visualisation/latent_space.py
+++ python/visualisation/latent_space.py
@@ -37,8 +37,8 @@ def plot_latent_space_in_dir(path, generate_images=True, save_path=None):
         MOVED_INDICES_FILE_NAME = f'distances{GEN_NUMBER}.dat'
         with open(MOVED_INDICES_FILE_NAME, "r") as f:
             lines = f.readlines()
-        lower_ball_moved = np.array([float(i) for i in lines[1].strip().split()[:-1]]) > 1e-6
-        higher_ball_moved = np.array([float(i) for i in lines[2].strip().split()[:-1]]) > 1e-6
+        lower_ball_moved = np.array([float(i) for i in lines[3].strip().split()[:-1]]) > 1e-6
+        higher_ball_moved = np.array([float(i) for i in lines[4].strip().split()[:-1]]) > 1e-6
         is_moved = np.logical_or(lower_ball_moved, higher_ball_moved)
 
         fig = plt.figure(figsize=(15, 15))
