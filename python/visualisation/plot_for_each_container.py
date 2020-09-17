2eafaac935d16a07f899c80df3819faebaa53368
Andy Wang <aw1912@ic.ac.uk>
2020-09-17 11:28:54 +0100

    adding new plots to plotforeachcontainer

--- python/visualisation/plot_for_each_container.py
+++ python/visualisation/plot_for_each_container.py
@@ -4,8 +4,9 @@ import shutil
 from ae_loss_AE import plot_loss_in_dir_AE
 from ae_loss_VAE import plot_loss_in_dir_VAE
 from latent_space import plot_latent_space_in_dir
-from recon_notmoved_var import plot_recon_not_moved_var_in_dir
 from latent_density import plot_latent_density_in_dir
+from pos_var import plot_pos_var_in_dir
+from pct_move import plot_pct_move_in_dir
 
 current_path = os.getcwd()
 sys.path.append("/git/sferes2/exp/imagesd/python/visualisation")
@@ -19,8 +20,9 @@ is_full_loss = "true" in sys.argv[3]
 
 print(f"PROCESSING VISUALISATIONS - {path}")
 plot_latent_space_in_dir(path)
-plot_recon_not_moved_var_in_dir(path, GENERATE_EACH_IMAGE)
 plot_latent_density_in_dir(path, GENERATE_EACH_IMAGE)
+plot_pos_var_in_dir(path, GENERATE_EACH_IMAGE)
+plot_pct_move_in_dir(path, GENERATE_EACH_IMAGE)
 
 # PID level plotting
 if variant == "vae":
